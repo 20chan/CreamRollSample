dd8b0ae1ba0c32dbaac8c69fe63b3d88ba08dd2d
20chan <2@0chan.dev>
2019-06-04 16:04:11 +0900

    Add sample code

---
+++ CreamRollSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CreamRoll;
+
+using static CreamRoll.RouteServer;
+
+namespace CreamRollSample {
+    class Program {
+        static void Main(string[] args) {
+            var counter = new Counter();
+            var server = new RouteServer<Counter>(counter);
+            server.StartAsync();
+
+            Console.Write("Press Enter twice to stop..");
+            Console.ReadLine();
+            Console.ReadLine();
+            server.Stop();
+            Console.WriteLine("Server stopped");
+        }
+    }
+
+    public class Counter {
+        private int counter = 0;
+
+
+        [Get("/")]
+        public string Count() {
+            return (++counter).ToString();
+        }
+
+        [Get("/async")]
+        public Task<string> AsyncCount() {
+            return Task.FromResult((++counter).ToString());
+        }
+    }
+}
